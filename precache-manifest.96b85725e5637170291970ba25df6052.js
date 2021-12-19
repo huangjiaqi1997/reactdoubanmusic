41c5e1403eb3c308915d1067c31436c418b1d5e1
huangjiaqi1997 <2931915202@qq.com>
2021-12-19 19:40:27 +0800

    Updates

---
+++ precache-manifest.96b85725e5637170291970ba25df6052.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d1876052cd88f6d05b",
+    "url": "/reactdoubanmusic/static/css/main.0399339d.chunk.css"
+  },
+  {
+    "revision": "87d1876052cd88f6d05b",
+    "url": "/reactdoubanmusic/static/js/main.87d18760.chunk.js"
+  },
+  {
+    "revision": "c0ab40de69ab7a09d40d",
+    "url": "/reactdoubanmusic/static/js/1.c0ab40de.chunk.js"
+  },
+  {
+    "revision": "a6f77c7ae30dbf32d68d",
+    "url": "/reactdoubanmusic/static/js/runtime~main.a6f77c7a.js"
+  },
+  {
+    "revision": "0237dc7e6fe759a6c166eef0b44c5efe",
+    "url": "/reactdoubanmusic/index.html"
+  }
+];
